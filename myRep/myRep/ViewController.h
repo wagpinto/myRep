25b54eade8c82bd06e45584089bebbd028b4488f
wagppinto <wagpinto@me.com>
2015-05-05 11:58:11 -0600

    Initial Commit
    Creating the iOS project on Xcode.
    
    Signed-off-by: wagppinto <wagpinto@me.com>

---
+++ myRep/myRep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myRep
+//
+//  Created by Wagner Pinto on 5/5/15.
+//  Copyright (c) 2015 weeblu.co llc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ myRep/myRep/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  myRep
+//
+//  Created by Wagner Pinto on 5/5/15.
+//  Copyright (c) 2015 weeblu.co llc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
